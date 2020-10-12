81d2ceeee9e400345a1f12d4d5e6531a83cdd273
IvanVlad1997 <ivanvlad97@yahoo.ro>
2020-10-12 20:22:23 +0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.modules';
 import { AppComponent } from './app.component';
+import { UserInputsComponent } from './user-inputs/user-inputs.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInputsComponent,
+    PipesComponent,
+    ServicesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-inputs/user-inputs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-inputs',
+  templateUrl: './user-inputs.component.html',
+  styleUrls: ['./user-inputs.component.css']
+})
+export class UserInputsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
